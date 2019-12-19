9052122eaec56df878b2307e0e63cc62fa855914
beld78 <belfami@gmail.com>
2019-12-19 17:19:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def index():
 
     printHtml = printHtml + '</table>'
 
-    return ""
+    return printHtml
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
