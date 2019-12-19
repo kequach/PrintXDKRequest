8f6f2c1a44acb70c0a945d200598ec5f436c027b
beld78 <belfami@gmail.com>
2019-12-19 15:48:12 +0900

    init2

---
+++ app.py
@@ -0,0 +1,51 @@
+#!flask/Scripts/python.exe
+from flask import Flask, jsonify, abort, request
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/')
+def hello():
+    return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/XDK', methods=['POST'])
+def print_data():
+    if not request.json:
+        abort(400)
+    print(request.json['hello'])
+    '''
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }'''
+    return jsonify({'test': request.json['hello']}), 201
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0', port="5000")
