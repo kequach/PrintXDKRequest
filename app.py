6b1ad4317f34b9623a5d633d764403ef9f94dc33
beld78 <belfami@gmail.com>
2020-01-29 15:54:27 +0900

    clean up

--- app.py
+++ app.py
@@ -3,28 +3,10 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 lightList = []
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
 
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
+@app.route('/example')
+def example():
+    return "Hello, World!"
 
 @app.route('/XDK', methods=['POST'])
 def printXDK():
@@ -37,13 +19,7 @@ def printXDK():
     # Return the response in json format
     return light
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.json['name']
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-
-# A welcome message to test our server
+# Browser print
 @app.route('/')
 def index():
     return render_template('index.html', lightList=lightList)
